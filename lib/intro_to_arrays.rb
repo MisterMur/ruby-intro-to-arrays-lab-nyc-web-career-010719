d2588227157cafe03f54647380c1549c23ac98e4
Brian Murillo <brian.j.murillo@gmail.com>
2018-12-17 01:40:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element(arr)
+  arr.first
+  
+end
+
+def third_element(arr)
+  
+  arr[2]
+end
+
+def array_with_two_elements
+  arr[0,1]
+end
+
+def instantiate_new_array
+  
+  []
+end
+
